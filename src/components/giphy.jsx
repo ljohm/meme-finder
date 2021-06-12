ddff1533590f439011e1d1dda46a038a285eadc1
yoon1111116 <yunn1209@gmail.com>
2021-06-12 17:06:51 +0900

    set search logic

--- src/components/giphy.jsx
+++ src/components/giphy.jsx
@@ -8,6 +8,9 @@ const Giphy = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(25);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,6 +20,7 @@ const Giphy = () => {
         const res = await axios("https://api.giphy.com/v1/gifs/trending", {
           params: {
             api_key: process.env.REACT_APP_GIPHY_API_KEY,
+            limit: 100,
           },
         });
         setData(res.data.data);
@@ -30,6 +34,31 @@ const Giphy = () => {
     fetchData();
   }, []);
 
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError(false);
+    setLoading(true);
+
+    try {
+      const res = await axios("https://api.giphy.com/v1/gifs/search", {
+        params: {
+          api_key: process.env.REACT_APP_GIPHY_API_KEY,
+          q: search,
+          limit: 100,
+        },
+      });
+      setData(res.data.data);
+    } catch (err) {
+      setError(true);
+      setTimeout(() => setError(false), 3000);
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="m-2">
       {error && (
@@ -40,6 +69,22 @@ const Giphy = () => {
           Unable to get Gifs, please try again in a few minutes.
         </div>
       )}
+      <form className="form-inline justify-content-center m-2">
+        <input
+          className="form-control"
+          type="text"
+          placeholder="search"
+          onChange={handleSearchChange}
+          value={search}
+        />
+        <button
+          onClick={handleSubmit}
+          type="submit"
+          className="btn btn-primary mx-2"
+        >
+          Go
+        </button>
+      </form>
       <div className="container gifs">
         {!loading &&
           data.map((el) => (
